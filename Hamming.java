c4da47393df7d2c37f98aed9cd8e566c510b789f
Liby99 <liby99@icloud.com>
2016-09-30 20:41:43 -0700

    Added some comments

--- Hamming.java
+++ Hamming.java
@@ -44,58 +44,79 @@ public class Hamming {
         //Save all the useful data
         int[] data = {encoded[2], encoded[4], encoded[5], encoded[6]};
         int[] parity = {encoded[0], encoded[1], encoded[3]};
+        
+        //Generate the expected correctParity based on the 4 digit data
         int[] correctParity = getParity(data);
         
         //Compare the given parity and the correct parity
+        //If like original p1 equals expected p1, then comparison[0] is true
         boolean[] comparison = {false, false, false};
         for (int i = 0; i < comparison.length; i++) {
             comparison[i] = parity[i] == correctParity[i];
         }
         
-        //Check if there's an error in the data field, and save that position in the
+        //Check if there's an error in the encoded array
+        //And save that position in the incorrectPosition
         int incorrectPosition = -1;
-        if (!comparison[0] && comparison[1] && comparison[2]) {
+        if (!comparison[0] && comparison[1] && comparison[2]) { //p1 wrong
             incorrectPosition = 0;
         }
-        if (comparison[0] && !comparison[1] && comparison[2]) {
+        if (comparison[0] && !comparison[1] && comparison[2]) { //p2 wrong
             incorrectPosition = 1;
         }
-        if (!comparison[0] && !comparison[1] && comparison[2]) { //p1, p2 wrong. p3 correct
+        if (!comparison[0] && !comparison[1] && comparison[2]) { //p1, p2 wrong
             incorrectPosition = 2;
         }
-        if (comparison[0] && comparison[1] && !comparison[2]) {
+        if (comparison[0] && comparison[1] && !comparison[2]) { //p3 wrong
             incorrectPosition = 3;
         }
-        if (!comparison[0] && comparison[1] && !comparison[2]) { //p1, p3 wrong. p2 correct
+        if (!comparison[0] && comparison[1] && !comparison[2]) { //p1, p3 wrong
             incorrectPosition = 4;
         }
-        if (comparison[0] && !comparison[1] && !comparison[2]) { //p2, p3 wrong. p1 correct
+        if (comparison[0] && !comparison[1] && !comparison[2]) { //p2, p3 wrong
             incorrectPosition = 5;
         }
         if (!comparison[0] && !comparison[1] && !comparison[2]) { //all three are wrong
             incorrectPosition = 6;
         }
         
+        //Check if there's an incorrect digit
         if (incorrectPosition != -1) {
             
+            //Flip the digit on the [incorrectPosition] of the 8 digit encoded code
             encoded[incorrectPosition] = encoded[incorrectPosition] == 0 ? 1 : 0;
             
+            //Ok we now flipped the digit.
+            //Then we need to calculate how many 1's are in the encoded array.
+            //This will be stored in the [sum]
             int sum = 0;
             for (int i = 0; i < encoded.length; i++) {
                 sum += encoded[i];
             }
             
+            //If there are even 1s, then we are good
+            //If there are odd 1s, then there must be two errors which we cannot fix
             if (sum % 2 == 0) {
+                
+                //Now the sum is even, so we are goo.
+                //Generate the data from the 8 digit encoded code
+                //And return the data
                 int[] decodedData = {encoded[2], encoded[4], encoded[5], encoded[6]};
                 return decodedData;
             }
             else {
+                
+                //No we are not good.
+                //Print the Error and return null.
                 System.out.println("Error Occurs, May Contain 2 Errors.");
                 return null;
             }
         }
         else {
             
+            //Ok so there are no error in the first 7 bits.
+            //And for now, it doesn't matter whether p8 is correct or not
+            //So just return the data!
             return data;
         }
     }
